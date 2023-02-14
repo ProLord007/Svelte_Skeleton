46261a2318dc97c74c655f91fb4790e1e2376818
ProLord007 <aurora.galaxy0820@gmail.com>
2023-02-13 20:04:49 -0500

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<div class="m-4 mt-9 flex h-full w-full flex-col">
-	<div class="flex w-full justify-center">
+<div class="flex flex-col w-full h-full m-4 mt-9">
+	<div class="flex justify-center w-full">
 		<p class="max-w-prose">
 			A (growing) collection of Skeleton examples that I came up with for my own enjoyment and
 			education.
